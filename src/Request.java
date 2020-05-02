ac0104119fad0066bdfbeadc2e8fade6cd7ad662
sinamna <sina3050@gmail.com>
2020-05-03 01:58:59 +0430

    work on list of requests

---
+++ src/Request.java
@@ -0,0 +1,41 @@
+import javax.swing.*;
+import javax.swing.tree.*;
+
+public class Request {
+    private RequestPanel requestPanel;
+    private ResponsePanel responsePanel;
+    private String requestName;
+    private String url;
+    private String Option;
+    public Request(String requestName){
+        requestPanel=new RequestPanel();
+        responsePanel=new ResponsePanel();
+        this.requestName=requestName;
+        this.Option="GET";
+    }
+    public RequestPanel getRequestPanel() {
+        return requestPanel;
+    }
+    public ResponsePanel getResponsePanel() {
+        return responsePanel;
+    }
+    public String getRequestName() {
+        return requestName;
+    }
+    public void setUrl(String url) {
+        this.url = url;
+    }
+    public void setOption(String option) {
+        Option = option;
+
+    }
+
+    public String getOption() {
+        return Option;
+    }
+
+    @Override
+    public String toString() {
+        return requestName;
+    }
+}
